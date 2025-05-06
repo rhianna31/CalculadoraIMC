ab9e92cac10b5da6a5832485e8ed62b8466351db
rhianna31 <rhiannagava09@gmail.com>
2025-05-06 11:26:43 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+function calculaMedia() {
+    const peso = parseFloat(document.getElementById("peso").value);
+    const altura = parseFloat(document.getElementById("altura").value);
+
+    IMC = (peso / (altura * altura)).toFixed(2)
+    let classificacao;
+
+    if (IMC < 18.5) {
+        classificacao = "abaixo do peso"
+    }else if (IMC <= 24.9) {
+        classificacao = "peso normal"
+    }else if (IMC <= 29.9) {
+        classificacao = "sobrepeso"
+    }else if (IMC <= 34.9) {
+        classificacao = "obesidade grau 1"
+    }else if (IMC <= 39.9) {
+        classificacao = "obesidade grau 2"
+    }else {
+        classificacao = "obesidade grau 3 (morbida)"
+    }
+
+    document.getElementById("IMC").innerText = `${IMC} - ${classificacao}`;
+
+}
